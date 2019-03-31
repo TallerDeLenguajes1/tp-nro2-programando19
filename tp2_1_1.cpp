55691b44e0eec58a412541a8521bcd26feaf073e
Silvia <48635878+programando19@users.noreply.github.com>
2019-03-31 01:08:03 -0300

    Add files via upload

--- tp2_1_1.cpp
+++ tp2_1_1.cpp
@@ -9,12 +9,12 @@ int main(){
 	int f,c; 
 	double mt[N][M] = {{1.0,2.0,3.0,4.0,5.0},{2.30,6.32,5.2145,9.2541,6.321},{2,3,8,2.8596,23.56,},{2.01,3.20,8.214,6.321,45.21}}; 
     
-    printf("\n\t\t****MATRIZ QUE ACCEDE A LOS ELEMENTOS DE UNA MATRIZ****\n\n");
+    printf("\n\t\t****CODIGO QUE ACCEDE A LOS ELEMENTOS DE UNA MATRIZ****\n\n");
     printf("\t\t\t**** Contiene Elementos de Prueba ****\n\n");
 
     for(f = 0;f<N; f++){       
-    	for(c = 0;c<N; c++){ 
-    	    printf(" | posicion[%d][%d]=%.2lf ",f,c, mt[f][c]);    
+    	for(c = 0;c<M; c++){ 
+    	    printf(" | posicion[%d][%d]=%.2lf ",f+1,c+1, mt[f][c]);    
     	}   
     printf("\n\n"); 
     } 
